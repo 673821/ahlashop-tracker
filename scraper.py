7665fd15b53c19d7c68f3761ac87d33348c1d1af
673821 <87588867+673821@users.noreply.github.com>
2026-06-08 16:39:06 +0100

    Enhance scraper with HTML parsing and message length limit
    Added time import and modified send function to limit message length to 4000 characters. Implemented HTML parsing to extract and send unique class names from the webpage.

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
-import os, requests
+import os, requests, time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
 
 TOKEN   = os.environ.get("TELEGRAM_TOKEN", "CHANGE_MOI")
 CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "7975203420")
@@ -10,7 +11,7 @@ CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "7975203420")
 def send(msg):
     requests.post(
         f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-        json={"chat_id": CHAT_ID, "text": msg, "parse_mode": "HTML"},
+        json={"chat_id": CHAT_ID, "text": msg[:4000], "parse_mode": "HTML"},
         timeout=10
     )
 
@@ -22,13 +23,20 @@ opts.add_argument("--window-size=1920,1080")
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opts)
 driver.get("https://ahlashop.net/shop/")
-
-import time
 time.sleep(5)
-
 html = driver.page_source
 driver.quit()
 
-# ابعث أول 3000 حرف من HTML
-send(f"<pre>{html[:3000]}</pre>")
-print("HTML sent")
+soup = BeautifulSoup(html, "html.parser")
+
+# شوف كلشي موجود
+all_tags = set()
+for tag in soup.find_all(True):
+    classes = tag.get("class", [])
+    for c in classes:
+        if any(x in c for x in ["product","price","woo","item","card"]):
+            all_tags.add(f"{tag.name}.{c}")
+
+msg = "🔍 Classes trouvées:\n" + "\n".join(list(all_tags)[:50])
+send(msg)
+print("Done")
